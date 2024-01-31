909742691ccf07589cbd05c9a1244abf43a9e639
Mindy D Tran <mindytran98@gmail.com>
2024-01-31 13:52:36 -0500

    copy code from abe

--- src/components/MovieDisplay.js
+++
@@ -1,4 +0,0 @@
-export default function MovieDisplay(props) {
-    // The component must return some JSX
-    return <h1>The MovieDisplay Component</h1>;
-  };
--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,15 +1,20 @@
+import { Link } from "react-router-dom";
+
 function MovieItem({ movie }) {
-  const { title, poster } = movie;
+  const { title, poster, _id } = movie;
+
   return (
     <div>
-      <h3>{title}</h3>
+      <Link to={`/movies/${_id}`}> 
+        <h3>{ title}</h3>
+      </Link>
+
       {poster ? (
         <img src={poster} alt={title} />
       ) : (
         <img
-          style={{width: 300+"px"}}
           src="https://cdn2.vectorstock.com/i/1000x1000/88/26/no-image-available-icon-flat-vector-25898826.jpg"
-          alt="no img"
+          alt=""
         />
       )}
     </div>
