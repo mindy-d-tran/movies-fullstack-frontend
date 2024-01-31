d103751b663c04e7738f397380b73606014798d0
Mindy D Tran <mindytran98@gmail.com>
2024-01-31 10:10:20 -0500

    change img size of no image to 300px width

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -7,6 +7,7 @@ function MovieItem({ movie }) {
         <img src={poster} alt={title} />
       ) : (
         <img
+          style={{width: 300+"px"}}
           src="https://cdn2.vectorstock.com/i/1000x1000/88/26/no-image-available-icon-flat-vector-25898826.jpg"
           alt="no img"
         />
