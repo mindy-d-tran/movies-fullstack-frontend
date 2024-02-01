eb9aa8ce8c883fd5d1606cc778e2d7212aac472c
Mindy D Tran <mindytran98@gmail.com>
2024-02-01 15:52:43 -0500

    adding light and dark mode

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,17 +1,25 @@
+import { useContext } from "react";
 import MovieItem from "./MovieItem";
 import Spinner from "react-bootstrap/Spinner";
+import { ThemeContext } from "../context/ThemeContext";
 
 function MoviesList({ movies }) {
+  const themeCtx = useContext(ThemeContext);
+  const {theme} = themeCtx;
+
   return (
-    <div>
-      <h2>Movie List</h2>
-      <section>
-        {movies.length ? (
-          movies.map((movie) => <MovieItem key={movie._id} movie={movie} />)
+    // <div style={{backgroundColor: theme==='light'? '#fff': '#000'}}>
+    <div className={theme === 'light'? 'lightMode': 'darkMode'}>
+      <h1>Movie List Component</h1>
+      <div>
+        {movies.length >= 1 ? (
+          movies.map((movie) => <MovieItem movie={movie} key={movie._id} />)
         ) : (
-          <Spinner animation="border"/>
+          <Spinner animation="grow" variant="info" role="status" as="span">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
         )}
-      </section>
+      </div>
     </div>
   );
 }
